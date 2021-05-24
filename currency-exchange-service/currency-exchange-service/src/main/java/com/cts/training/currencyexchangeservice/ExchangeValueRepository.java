6a5dd04d4b6105d45d935593e1e716eea7a48adf
srisuresh102 <80826015+srisuresh102@users.noreply.github.com>
2021-05-24 13:10:03 +0530

    currency exchange & conversion @step 20

---
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/cts/training/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.cts.training.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends 
+JpaRepository<ExchangeValue, Long> {
+	ExchangeValue findByFromAndTo(String from, String to);
+
+}
